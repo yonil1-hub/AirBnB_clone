3016766f2d142fe362342509c8e75e22ccb1e03e
kirubel <kirubel@gmail.com>
2022-09-03 05:48:24 -0700

    task seconed commit

--- models/base_models.py
+++ models/base_models.py
@@ -6,17 +6,32 @@ from datetime import datetime
 class BaseModel:
     """base class for all class"""
 
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, id, *args, **kwargs):
         """ initilizes attribute for class
         Args
         id : unique identifier
         created_at: datetime object
         uppdated_at: date time object
         """
+        if kwargs and kwargs != {}:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    setattr(k, datetime.strptime(v, "%y-%m-%dT%H:%M:%s.%f"))
+                elif k == '__class__':
+                    continue
+                else:
+                    setattr(self, k, v)
+        else:
+            self.id = uuid4()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
-        self.id = uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __str__(self):
+        """Returns a formated string of classname, id, and dictionary
+        contens
+        """
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def seve(self):
         """saves instances"""
