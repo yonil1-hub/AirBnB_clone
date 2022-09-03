22324b1e91b5ce5bbe35e730d60e0da11487914f
kirubel <kirubel@gmail.com>
2022-09-03 06:54:16 -0700

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,37 @@
 #!/usr/bin/python3
 """model that holede Basemodel class"""
- import uuid
- from datetime import datetime
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    """Base class"""
+    """base model class"""
 
-    def __init__(self, *args, **kwargs):
-        """Initializes attributes for class BaseModel
-        Args:
-            id - identification number
-            *args - arguments (not used)
-            **kwargs - dictionary arguments
-        """
-
-        if kwargs and kwargs != {}:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    setattr(self, k, datetime.strptime(v,
-                                                       "%Y-%m-%dT%H:%M:%S.%f"))
-                elif k == "__class__":
-                    continue
-                else:
-                    setattr(self, k, v)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+    def __init__(self, id, created_at, updated_at):
+        """ intiate attributs"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns a formated string of classname, id, and dictionary
-        contens
+        """Return a formated strining format string of clasname, id
+        created_at and updated_at
         """
 
         return ("[{}] ({}) {}".format(self.__class__.__name__,
                                       self.id, self.__dict__))
 
-    def save(self):
-        """Saves any new information added to a class instance and
-        saves an update time
-        """
+    def seve(self):
+        """save new information add in BassModel class"""
 
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary of all the contens of a class instance"""
-
+        """ eturns a dictionary containing
+        all keys/values of __dict__ of the instance
+        """
         d = self.__dict__.copy()
-        d['__class__'] = self.__class__.__name__
-        d['created_at'] = self.created_at.isoformat()
+        d['__class__'] = self.__class__.name
         d['updated_at'] = self.updated_at.isoformat()
+        d['created_at'] = self.created_at.isoformat()
         return d
