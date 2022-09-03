923d30a592262b01c54e4aafaa28810541333862
kirubel <kirubel@gmail.com>
2022-09-03 07:00:08 -0700

    task4

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,29 @@
 """model that holede Basemodel class"""
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """base model class"""
 
-    def __init__(self, id, created_at, updated_at):
-        """ intiate attributs"""
+    def __init__(self, *args, **kwargs):
+        """Initializes attributes for class BaseModel
+        Args:
+            id - identification number
+            *args - arguments (not used)
+            **kwargs - dictionary arguments
+        """
+
+        if kwargs and kwargs != {}:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    setattr(self, k, datetime.strptime(v,
+                                                       "%Y-%m-%dT%H:%M:%S.%f"))
+                elif k == "__class__":
+                    continue
+                else:
+                    setattr(self, k, v)
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
